3d0d0d338af470300450567a290c06b3131a0650
deepak <deepakmethalayil@gmail.com>
2020-04-26 18:13:36 +0530

    added more excercises

---
+++ name_cases.py
@@ -0,0 +1,17 @@
+name= "eric"
+message= f"Hello {name.title()} ,  would you like to learn some Python today?",
+print(message)
+
+Person_Name = "Deepak  Methalayil"
+print(Person_Name.lower())
+print(Person_Name.upper())
+print(Person_Name.title())
+
+# excercise 2.5
+print("Albert Einstein once said, “A person who never made a mistake never tried anything new.”")
+
+#excercise 2-6
+
+famous_person = "Albert Einstein"
+message = f"{famous_person.title()} once said,  “A person who never made a mistake never tried anything new.”"
+print(message)
